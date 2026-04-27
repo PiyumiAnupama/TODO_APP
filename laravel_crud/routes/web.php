446d9ba9881e88e6c60ba92fea7cebba93602c44
PiyumiAnupama <96447@fhss.sjp.ac.lk>
2026-04-27 20:20:21 +0530

    fix: route and deprecation warning

--- laravel_crud/routes/web.php
+++ laravel_crud/routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('todo');
-});
-
+Route::get('/', [\App\Http\Controllers\TodoController::class, 'index']);
 Route::resource('todos', \App\Http\Controllers\TodoController::class);
 Route::get('todos/{todo}', [\App\Http\Controllers\TodoController::class,'destroy'])->name('todos.destory');
